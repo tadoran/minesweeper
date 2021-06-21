13a0eab2de05abfe52700ec0befc9d5c22862efd
Konstantin Gorelov <gorelovks@yandex.ru>
2021-06-21 20:57:49 +0300

    added executables

---
+++ build.bat
@@ -0,0 +1,4 @@
+G:\Qt\venv\Scripts\python.exe -m PyInstaller game.py --add-data=wav;wav --add-data=img;img --noconsole
+
+G:\Qt\venv\Scripts\python.exe -m PyInstaller game.py --add-data=wav;wav --add-data=img;img --noconsole -i game.ico
+
